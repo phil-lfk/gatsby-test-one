c81a4f008dff3579fd59ef9fcffd785dcd11181b
phil-lfk <p.viotti@lafabrik.nc>
2024-10-01 14:59:38 +1100

    mofis about et greeting

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,15 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <main>
-      <h1>A propos</h1>
-      <p>Salut, je débute sur Gatsby</p>
+      <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
 }
 
-export const Head = () => (
-    <>
-      <title>A propos</title>
-      <meta name="description de la page" content="Ma propre description" />
-    </>
-  )
-
+export const Head = () => <title>About Me</title>
 export default AboutPage
