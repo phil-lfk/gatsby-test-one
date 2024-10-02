317df803a6ddfc6e2c04e6912ba36c89942d5df1
phil-lfk <p.viotti@lafabrik.nc>
2024-10-02 13:55:13 +1100

    mise à jour blog part I

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,23 @@
  */
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My First Gatsby Site &&&&",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extension: [".mdx", ".md"],
+      },
+    },
   ],
 };
