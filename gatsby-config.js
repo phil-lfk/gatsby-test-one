55de1c0b7132f49a9625458bb82524ca7db0fa34
phil-lfk <p.viotti@lafabrik.nc>
2024-10-02 14:52:14 +1100

    ajout des extraits - Blog Part II

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site &&&&",
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/blog/`,
       },
     },
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        extension: [".mdx", ".md"],
-      },
-    },
+    "gatsby-plugin-mdx",
   ],
 };
